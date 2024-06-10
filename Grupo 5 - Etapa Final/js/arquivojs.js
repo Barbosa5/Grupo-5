33dbab64a7c8f33d15103f225c21168c35107af2
Barbosa5 <130063524+Barbosa5@users.noreply.github.com>
2024-06-10 11:39:30 -0300

    Add files via upload

---
+++ Grupo 5 - Etapa Final/js/arquivojs.js
@@ -0,0 +1,12 @@
+//Criando Modo Escuro.
+function modoEscuro(){
+    alert('Você está mudando para o modo escuro');
+    window.document.bgColor='black';
+    document.fgColor='white';
+}
+//Criando Modo Claro.
+function modoClaro(){
+    alert('Você esta mudando para o modo claro');
+    window.document.bgColor='white';
+    document.fgColor='black';
+}
